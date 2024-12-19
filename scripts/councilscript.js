6d1d298660484af336aa49b56720e84ee63ec0ac
Prince Raj <145830910+princerxj@users.noreply.github.com>
2024-12-19 08:20:23 +0530

    Update councilscript.js

--- scripts/councilscript.js
+++ scripts/councilscript.js
@@ -39,7 +39,7 @@ fetch('/IIC/Data/faculty-section.json')
     console.error('Error:', error);
 });
 
-fetch('/IIC/Data/student-members.json?cache_bust=' + new Date().getTime())
+fetch('/IIC/Data/student-members.json')
   .then(response => {
     console.log('Fetching URL:', response.url); 
     if (!response.ok) {
